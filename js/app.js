078fe93b42195e176c9570c42cafd484dbbd23c9
Jeremy Chon <Jeremy@Jeremys-MacBook-Pro.local>
2019-07-09 15:52:50 -0500

    obstacles are moving

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ class Ship {
 		this.y = 680;
 		this.r = 25;
 		this.color = 'black';
-		this.speed = 4;
+		this.speed = 7;
+		this.isAlive = true
 		this.direction = {
 			// up: false,
 			left: false,
@@ -48,22 +49,49 @@ class Ship {
 		if (key == 'ArrowDown') this.direction.down = false;
 		if (key == 'ArrowRight') this.direction.right = false;
 	}
+
+	gameOver () {
+		if (this.isAlive === true) {
+			console.log('Keep going');
+		} else {
+			document.write(`<h1>Game Over</h1>`)
+		}
+	}
 }
 
 class Obstacle {
 	constructor () {
 		this.x = 0;
-		this.y = 400;
+		this.y = 0;
 		this.height = 20;
-		this.width = 400;
-		this.color = "green"
+		this.width = Math.floor(Math.random() * (400 - 100 + 1)) + 100;
+		this.gap = Math.floor(Math.random() * (200 - 100 + 1)) + 100
+		this.color = "green";
+		this.speed = 3;
 	}
 
-	makeObstacle () {
+	makeLeftObstacle () {
 		ctx.beginPath()
 		ctx.rect(this.x, this.y, this.width, this.height)
 		ctx.fillStyle = this.color
 		ctx.fill()
+	}
+
+	makeRightObstacle () {
+		ctx.beginPath()
+		ctx.rect(this.width + this.gap, this.y, canvas.width - (this.width + this.gap) , this.height)
+		ctx.fillStyle = this.color
+		ctx.fill()
+	}
+
+	fallLeft () {
+		this.y += this.speed
+		this.makeLeftObstacle()
+	}
+
+	fallRight () {
+		this.y += this.speed		
+		this.makeRightObstacle()
 	}
 }
 
@@ -72,18 +100,36 @@ class Obstacle {
 const game = {
 
 	ship: null,
-	block: null,
+	leftBlock: [],
+	rightBlock: [],
+	frame: 0,
+	intervalID: null,
 
 	playGame () {
 		const player = new Ship()
 		player.makeShip()
-		const obstacle = new Obstacle()
-		obstacle.makeObstacle();
 		this.ship = player
-		this.block = obstacle
 		console.log(this.ship);
-		console.log(obstacle.makeObstacle);
-		animate()
+
+		this.setTimer();
+	},
+
+	setTimer () {
+		this.intervalID = setInterval( () => {
+			const obstacle = new Obstacle()
+			obstacle.makeLeftObstacle();
+			this.leftBlock.push(obstacle);
+			// console.log(this.leftBlock);
+			this.frame = this.leftBlock.length
+		}, 1000)
+
+		this.intervalID = setInterval( () => {
+			const obstacle = new Obstacle()
+			obstacle.makeRightObstacle();
+			this.rightBlock.push(obstacle);
+			// console.log(this.rightBlock);
+			this.frame = this.rightBlock.length
+		}, 1000)
 	},
 
 	clearCanvas () {
@@ -104,13 +150,9 @@ const game = {
 		let distance = Math.sqrt((distX*distX) + (distY*distY))
 
 		if (distance <= ship.r) {
-			return true
+			return true; 
 		}
 	},
-
-	gameOver () {
-		document.write(`<h1>Game Over</h1>`)
-	}
 
 	// 
 
@@ -118,24 +160,34 @@ const game = {
 
 game.playGame()
 
-
 let x = 0;
 function animate () {
+	// console.log(x++);
 	game.ship.moveShip();
 	game.clearCanvas();
 	game.ship.makeShip();
 
-	game.block.makeObstacle()
 
-	if (game.checkCollision(game.ship, game.block)) {
-		game.gameOver()
-		return;
-	} else {
-		window.requestAnimationFrame(animate)
+	// every 
+	for (let i = 0; i < game.frame; i++) {
+		game.leftBlock[i].fallLeft()
+		game.rightBlock[i].fallRight()
+		if (game.checkCollision(game.ship, game.leftBlock[i])) {
+			game.ship.isAlive = false
+			game.ship.gameOver()
+			return;
+		}
+		if (game.checkCollision(game.ship, game.rightBlock[i])) {
+			game.ship.isAlive = false
+			game.ship.gameOver()
+			return;
+		}
 	}
+			window.requestAnimationFrame(animate)
+
 }
 
-// animate()
+animate()
 
 
 
